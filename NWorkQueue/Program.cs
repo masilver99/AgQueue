0d0d77ce8f782e65835ac8c75b4d76634074ad99
Michael Silver <masilver@gmail.com>
2018-01-05 18:02:27 -0500

    Add project files.

---
+++ NWorkQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NWorkQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
