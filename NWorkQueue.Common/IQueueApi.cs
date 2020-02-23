66883ea3799631a1b9f88a142f90f82faa03af03
Michael Silver <masilver@gmail.com>
2020-02-23 02:16:09 -0500

    Adding gRPC as server type

---
+++ NWorkQueue.Common/IQueueApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace NWorkQueue.Common
+{
+    [ServiceContract(Name = "")]
+    interface IQueueApi
+    {
+        ValueTask<>
+    }
+}
---
+++ NWorkQueue.Server/QueueApi.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NWorkQueue.Server
+{
+    public class QueueApi : IQueueApi
+    {
+        ValueTask Test()
+        {
+            throw new RpcException
+        }
+        //CreateQueue
+        //DeleteQueue
+        //AddMessage
+        //PeekMessage
+        //PullMessage
+    }
+}
