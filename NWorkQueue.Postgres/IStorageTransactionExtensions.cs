affe88736da55aea974f277a54c8fe4846e5da38
Michael Silver <masilver@gmail.com>
2020-05-03 02:18:13 -0400

    Adding Postgres backend

---
+++ NWorkQueue.Postgres/IStorageTransactionExtensions.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="IStorageTransactionExtensions.cs" company="Michael Silver">
+// Copyright (c) Michael Silver. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+using NWorkQueue.Server.Common;
+
+namespace NWorkQueue.Sqlite
+{
+    /// <summary>
+    /// Extensions on the IStorageTransaction interface.
+    /// </summary>
+    internal static class IStorageTransactionExtensions
+    {
+        /// <summary>
+        /// Converts the IStorageTransaction to a NpgsqlTransaction.
+        /// </summary>
+        /// <param name="iTrans">The IStorageTransaction to convert.</param>
+        /// <returns>SqliteTransaction.</returns>
+        public static NpgsqlTransaction NpgsqlTransaction(this IStorageTransaction iTrans)
+        {
+            return (iTrans as DbTransaction).NpgsqlTransaction;
+        }
+    }
+}
